2910bc11806daa00b665994168ce8d7e9fce0924
kaybee-mn <121321741+kaybee-mn@users.noreply.github.com>
2025-06-19 20:00:09 -0700

    feat: enhance UserContext to include avatar link management and alert handling

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,10 +1,36 @@
-import { createContext } from "react";
+import { createContext, ReactNode } from "react";
+import supabase from "../api/supabaseClient";
+import { useAlert } from "./AlertContext";
 
-const UserContext = createContext<{
-    avatarLink: string;
-    setAvatarLink: (newLink: string) => void;
-} | undefined>(undefined);
+const UserContext = createContext<
+  | {
+      getAvatarLink: () => string;
+      setAvatarLink: (newPath: string, file: File) => void;
+    }
+  | undefined
+>(undefined);
 
-export const setAvatarLink=(newLink : string)=>{
-    
-}
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const { triggerAlert } = useAlert();
+  const setAvatarLink = async (newPath: string, file: File) => {
+    const { error: uploadError } = await supabase.storage
+      .from("avatars")
+      .upload(newPath, file, {
+        upsert: true,
+      });
+
+    if (uploadError) {
+      triggerAlert("Upload failed!");
+      throw uploadError;
+    }
+  };
+  const getAvatarLink = () => {
+    return "link";
+  };
+
+  return (
+    <UserContext.Provider value={{ setAvatarLink, getAvatarLink }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
